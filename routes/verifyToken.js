056c2e928ef026f7ae96432206569a2144eb1c1f
Saroj Dangol <zorasdangol34@gmail.com>
2022-09-10 11:09:29 +0545

    [KV-6] Updated access token errormessage api

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -6,14 +6,14 @@ const jwt = require('jsonwebtoken');
 function tokenVerifier(req, res, next){
     const token = req.header('auth-token');
     if(!token){
-        return res.status(401).send('Access Denied');
+        return res.status(401).send({message:'Access Denied'});
     }
     try{
         const verified = jwt.verify(token, process.env.SECRET_TOKEN);
         req.user = verified;
         next();
     }catch(err){
-        res.status(400).send('Invalid Token');
+        res.status(400).send({message: 'Invalid Token'});
     }
 
 }
