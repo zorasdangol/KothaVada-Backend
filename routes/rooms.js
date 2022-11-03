11013dfa93d5fcca8a7d72de9dae453e905a9207
Saroj Dangol <zorasdangol34@gmail.com>
2022-11-03 21:36:19 +0545

    [1.0] Updated room api for patch

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,85 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Room = require('../models/Room');
-const tokenVerifier = require('./verifyToken');
-
+const Room = require("../models/Room");
+const tokenVerifier = require("./verifyToken");
 
 //get back all the rooms
-router.get('/', tokenVerifier,  async (req, res) => {
-    try{
-        const rooms = await Room.find({landlordId: req.user._id});
-        res.json(rooms);
-    }catch(err){
-        req.status(400).json({message: err});
-    }
+router.get("/", tokenVerifier, async (req, res) => {
+  try {
+    const rooms = await Room.find({ landlordId: req.user._id });
+    res.json(rooms);
+  } catch (err) {
+    req.status(400).json({ message: err });
+  }
 });
 
-
 //ADD A ROOM
-router.post('/', tokenVerifier, async (req, res) => {
-    console.log(req.body);
-    const room = createDataFromReqBody(req.body);
+router.post("/", tokenVerifier, async (req, res) => {
+  console.log(req.body);
+  const room = createDataFromReqBody(req.body);
 
-    //update landlordId in room 
-    room.landlordId = req.user._id;
-    await room.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch( err => {
-            console.log('test', err);
-            res.status(400).json({message: err});
-        });
+  //update landlordId in room
+  room.landlordId = req.user._id;
+  await room
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log("test", err);
+      res.status(400).json({ message: err });
+    });
 });
 
 //post method to return room details from roomId
-router.get('/:roomId',tokenVerifier,  async (req, res) => {
-    console.log(req.params.roomId);
-    try{
-        const room  = await Room.findById(req.params.roomId);
-        res.json(room);    
-    }catch(err){
-        res.status(400).json({message: err});
-    }
+router.get("/:roomId", tokenVerifier, async (req, res) => {
+  console.log(req.params.roomId);
+  try {
+    const room = await Room.findById(req.params.roomId);
+    res.json(room);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
-router.delete('/:roomId', async (req, res) => {
-    try{
-        const removedItem = await Room.remove({_id: req.params.roomId});
-        res.json(removedItem);
-    }catch(err){
-        res.status(400).json({message: err});
-    }
+router.delete("/:roomId", async (req, res) => {
+  try {
+    const removedItem = await Room.remove({ _id: req.params.roomId });
+    res.json(removedItem);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
 //update a post
-router.patch('/:roomId', async (req, res) => {
-    try{
-        const updatedRoom = await Room.updateOne(
-            {_id: req.params.roomId}, 
-            { $set: createDataFromReqBody(req.body)}
-            );
-        res.json(updatedRoom);
-    }catch(err){
-        res.status(400).json({message: err});
-    }
+router.patch("/:roomId", async (req, res) => {
+  try {
+    console.log("test here" + req.params.roomId);
+    const updatedRoom = createDataFromReqBody(req.body);
+    await Room.updateOne({ _id: req.params.roomId }, { $set: updatedRoom });
+    res.json(updatedRoom);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
 /*
  * method to convert req body data to room data
  */
 const createDataFromReqBody = (body) => {
-    const room = new Room({
-        roomName: body.roomName,
-        landlordId: body.landlordId,
-        tenantPhone: body.tenantPhone,
-        type: body.type,
-        occupancy: body.occupancy,
-        floor: body.floor,
-        price: body.price,
-        garbageCharge: body.garbageCharge
-    });
-    return room;
-}
+  const room = new Room({
+    roomName: body.roomName,
+    landlordId: body.landlordId,
+    tenantPhone: body.tenantPhone,
+    type: body.type,
+    occupancy: body.occupancy,
+    floor: body.floor,
+    price: body.price,
+    garbageCharge: body.garbageCharge,
+  });
+  if (body._id) {
+    room._id = body._id;
+  }
+  return room;
+};
 
 module.exports = router;
