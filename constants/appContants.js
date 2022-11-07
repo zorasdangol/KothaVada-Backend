382eb5451d57b8998d0ed3d552e48c511b1b7386
Saroj Dangol <zorasdangol34@gmail.com>
2022-11-07 23:39:13 +0545

    [KV-18] Updated tenant api for updating room when tenant is saved

--- constants/appContants.js
+++ constants/appContants.js
@@ -1,5 +1,10 @@
 module.exports.STATUS_PAYMENT = {
-    PAID : 'Paid',
-    UNPAID : "Unpaid",
-    PARTIAL: "Partial"
-}
+  PAID: "Paid",
+  UNPAID: "Unpaid",
+  PARTIAL: "Partial",
+};
+
+module.exports.STATUS_TENANT = {
+  ACTIVE: "Active",
+  INACTIVE: "InActive",
+};
--- routes/tenants.js
+++ routes/tenants.js
@@ -2,30 +2,70 @@ const express = require("express");
 const router = express.Router();
 const Tenant = require("../models/Tenant");
 const User = require("../models/User");
+const Room = require("../models/Room");
 const tokenVerifier = require("./verifyToken");
 
-//ADD A ROOM
+const conn = require("../services/connection");
+const { default: mongoose } = require("mongoose");
+const { STATUS_TENANT } = require("../constants/appContants");
+
+//ADD A Tenent
 router.post("/", tokenVerifier, async (req, res) => {
   console.log(req.body);
+  const session = await conn.startSession();
+  session.startTransaction();
   const tenant = createDataFromReqBody(req.body);
 
   //update user id in tenant
   if (tenant.tenantPhone) {
-    let user = User.find({ mobile: tenant.tenantPhone });
+    await session.commitTransaction();
+    let user = await User.findOne({ mobile: tenant.tenantPhone });
     if (user) {
       tenant.userId = user._id;
     }
   }
 
+  //deactivate tenants for room
+  const filter = { roomId: tenant.roomId, status: STATUS_TENANT.ACTIVE };
+  const updateTenant = {
+    $set: {
+      status: STATUS_TENANT.INACTIVE,
+    },
+  };
+  await session.commitTransaction();
+  const result = await Tenant.updateMany(filter, updateTenant);
+
   await tenant
     .save()
-    .then((data) => {
+    .then(async (data) => {
+      await session.commitTransaction();
+      //Update room for tenant
+      let room = await Room.findById(data.roomId);
+      room.tenantId = data._id;
+
+      await session.commitTransaction();
+      await Room.updateOne({ _id: room._id }, { $set: room });
+
       res.json(data);
     })
-    .catch((err) => {
+    .catch(async (err) => {
+      await session.abortTransaction();
+      session.endSession();
       console.log("test", err);
       res.json({ message: err });
     });
+});
+
+//post method to return tenant details from roomId
+router.get("/:roomId", tokenVerifier, async (req, res) => {
+  console.log(req.params.roomId);
+  try {
+    const room = await Tenant.find({ roomId: req.params.roomId });
+    console.log();
+    res.json(room);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
 //post method to return tenant details from tenantId
